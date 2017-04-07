0db8f0d95e76970e8b0d5930bb2edadd90001ee5
marlass <lasakmarcin90@gmail.com>
2017-04-07 13:28:22 +0200

    Remove lodash and change import

--- app/reducers/projects.js
+++ app/reducers/projects.js
@@ -1,9 +1,4 @@
-import _ from 'lodash';
-import { ADD_PROJECT, REMOVE_PROJECT, SET_PROJECT_NAME,
-    SET_PROJECT_DESCRIPTION, ADD_PROJECT_CATEGORY, REMOVE_PROJECT_CATEGORY,
-    SET_PROJECT_CATEGORY_NAME, SET_PROJECT_CATEGORY_DESCRIPTION,
-    EDIT_PROJECT_ID, EDIT_PROJECT_CATEGORY_ID
-} from '../types';
+import * as types from '../types';
 import Immutable from 'immutable';
 
 const projectsReducer = (
@@ -11,31 +6,31 @@ const projectsReducer = (
   action
 ) => {
   switch (action.type) {
-    case ADD_PROJECT:
+    case types.ADD_PROJECT:
       return state.set(action.payload, Immutable.Map({
         id: action.payload,
         name: '',
         description: '',
         categories: Immutable.Map({})
       }))
-    case REMOVE_PROJECT:
+    case types.REMOVE_PROJECT:
       return state.delete(action.payload);
-    case SET_PROJECT_NAME:
+    case types.SET_PROJECT_NAME:
       return state.set(action.payload.projectId,
         state
           .get(action.payload.projectId)
           .set('name',action.payload.name));
-    case SET_PROJECT_DESCRIPTION:
+    case types.SET_PROJECT_DESCRIPTION:
       return state.set(action.payload.projectId,
         state
           .get(action.payload.projectId)
           .set('description',action.payload.description));
-    case ADD_PROJECT_CATEGORY:
-    case REMOVE_PROJECT_CATEGORY:
-    case SET_PROJECT_CATEGORY_NAME:
-    case SET_PROJECT_CATEGORY_DESCRIPTION:
-    case EDIT_PROJECT_ID:
-    case EDIT_PROJECT_CATEGORY_ID:
+    case types.ADD_PROJECT_CATEGORY:
+    case types.REMOVE_PROJECT_CATEGORY:
+    case types.SET_PROJECT_CATEGORY_NAME:
+    case types.SET_PROJECT_CATEGORY_DESCRIPTION:
+    case types.EDIT_PROJECT_ID:
+    case types.EDIT_PROJECT_CATEGORY_ID:
       return state;
     default:
       return state;
