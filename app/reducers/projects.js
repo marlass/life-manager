7a24df1ec6700648b91ea704ffbc7b44eea3b56a
marlass <lasakmarcin90@gmail.com>
2017-05-14 06:05:04 +0200

    Add reducer cases

--- app/reducers/projects.js
+++ app/reducers/projects.js
@@ -1,50 +1,81 @@
-import Immutable from "immutable";
+import Immutable from 'immutable';
 
-const ADD_PROJECT = "ADD_PROJECT";
-const REMOVE_PROJECT = "REMOVE_PROJECT";
-const SET_PROJECT_NAME = "SET_PROJECT_NAME";
-const SET_PROJECT_DESCRIPTION = "SET_PROJECT_DESCRIPTION";
-const ADD_PROJECT_CATEGORY = "ADD_PROJECT_CATEGORY";
-const REMOVE_PROJECT_CATEGORY = "REMOVE_PROJECT_CATEGORY";
-const SET_PROJECT_CATEGORY_NAME = "SET_PROJECT_CATEGORY_NAME";
-const SET_PROJECT_CATEGORY_DESCRIPTION =
-  "SET_PROJECT_CATEGORY_DESCRIPTION";
-const EDIT_PROJECT_ID = "EDIT_PROJECT_ID";
-const EDIT_PROJECT_CATEGORY_ID = "EDIT_PROJECT_CATEGORY_ID";
+const ADD_PROJECT = 'ADD_PROJECT';
+const REMOVE_PROJECT = 'REMOVE_PROJECT';
+const SET_PROJECT_NAME = 'SET_PROJECT_NAME';
+const SET_PROJECT_DESCRIPTION = 'SET_PROJECT_DESCRIPTION';
+const ADD_PROJECT_CATEGORY = 'ADD_PROJECT_CATEGORY';
+const REMOVE_PROJECT_CATEGORY = 'REMOVE_PROJECT_CATEGORY';
+const SET_PROJECT_CATEGORY_NAME = 'SET_PROJECT_CATEGORY_NAME';
+const SET_PROJECT_CATEGORY_DESCRIPTION = 'SET_PROJECT_CATEGORY_DESCRIPTION';
+const EDIT_PROJECT_ID = 'EDIT_PROJECT_ID';
+const EDIT_PROJECT_CATEGORY_ID = 'EDIT_PROJECT_CATEGORY_ID';
 
 const projectsReducer = (state = Immutable.Map({}), action) => {
   switch (action.type) {
     case ADD_PROJECT:
-      return state.set(
-        action.payload,
-        Immutable.Map({
-          id: action.payload,
-          name: "",
-          description: "",
-          categories: {},
-        }),
-      );
+      return state.set(action.payload, {
+        id: action.payload,
+        name: '',
+        description: '',
+        categories: {},
+      });
     case REMOVE_PROJECT:
       return state.delete(action.payload);
     case SET_PROJECT_NAME:
       return state.set(
         action.payload.projectId,
-        state.get(action.payload.projectId).set("name", action.payload.name),
+        state.get(action.payload.projectId).set('name', action.payload.name)
       );
     case SET_PROJECT_DESCRIPTION:
       return state.set(
         action.payload.projectId,
         state
           .get(action.payload.projectId)
-          .set("description", action.payload.description),
+          .set('description', action.payload.description)
       );
     case ADD_PROJECT_CATEGORY:
+      return state.set(
+        action.payload.projectId,
+        state.get('categories').set(action.payload.categoryId, {})
+      );
     case REMOVE_PROJECT_CATEGORY:
+      return state.set(
+        action.payload.projectId,
+        state.get('categories').delete(action.payload.categoryId)
+      );
     case SET_PROJECT_CATEGORY_NAME:
+      return state.setIn(
+        [
+          action.payload.projectId,
+          'categories',
+          action.payload.categoryId,
+          'name',
+        ],
+        action.payload.name
+      );
     case SET_PROJECT_CATEGORY_DESCRIPTION:
+      return state.setIn(
+        [
+          action.payload.projectId,
+          'categories',
+          action.payload.categoryId,
+          'description',
+        ],
+        action.payload.description
+      );
     case EDIT_PROJECT_ID:
+      const map = state.mapKeys(k => {
+        if (k === action.payload.projectId) return action.payload.id;
+        return k;
+      });
+      return map;
     case EDIT_PROJECT_CATEGORY_ID:
-      return state;
+      const map2 = state[action.payload.projectId].categories.mapKeys(k => {
+        if (k === action.payload.categoryId) return action.payload.id;
+        return k;
+      });
+      return map;
     default:
       return state;
   }
@@ -120,7 +151,7 @@ export function setProjectCategoryName(projectId, categoryId, name) {
 export function setProjectCategoryDescription(
   projectId,
   categoryId,
-  description,
+  description
 ) {
   return {
     type: SET_PROJECT_CATEGORY_DESCRIPTION,
