01547851ce8c3393eda60ae9f211d4a06217843e
marlass <lasakmarcin90@gmail.com>
2017-05-24 10:37:09 +0200

    Change app title

--- app/pages/assets.js
+++ app/pages/assets.js
@@ -1,6 +1,3 @@
-/* Based on the template in Web Starter Kit :
-https://github.com/google/web-starter-kit/blob/master/app/index.html
-*/
 import chromeFavicon from "../images/chrome-ninja192-precomposed.png";
 import appleFavicon from "../images/apple-ninja152-precomposed.png";
 import msFavicon from "../images/ms-ninja144-precomposed.png";
@@ -10,22 +7,11 @@ import { isProduction } from "../../config/app";
 const metaAssets = () => {
   return [
     { charset: "utf-8" },
-    // Meta descriptions are commonly used on search engine result pages to
-    // display preview snippets for a given page.
     {
       name: "description",
-      content: "Your One-Stop solution for a full-stack universal Redux App",
+      content: "Manage you most important tasks of your life",
     },
-    // Setting IE=edge tells Internet Explorer to use the latest engine to
-    //  render the page and execute Javascript
     { "http-equiv": "X-UA-Compatible", content: "IE=edge" },
-    // Using the viewport tag allows you to control the width and scaling of
-    // the browser's viewport:
-    // - include width=device-width to match the screen's width in
-    // device-independent pixels
-    // - include initial-scale=1 to establish 1:1 relationship between css pixels
-    // and device-independent pixels
-    // - ensure your page is accessible by not disabling user scaling.
     { name: "viewport", content: "width=device-width, initial-scale=1" },
     // Disable tap highlight on IE
     { name: "msapplication-tap-highlight", content: "no" },
@@ -34,7 +20,7 @@ const metaAssets = () => {
     // Add to homescreen for Safari on IOS
     { name: "apple-mobile-web-app-capable", content: "yes" },
     { name: "apple-mobile-web-app-status-bar-style", content: "black" },
-    { name: "apple-mobile-web-app-title", content: "reactGo" },
+    { name: "apple-mobile-web-app-title", content: "Life manager" },
     // Tile icon for Win8 (144x144 + tile color)
     { name: "msapplication-TileImage", content: msFavicon },
     { name: "msapplication-TileColor", content: "#3372DF" },
@@ -53,14 +39,11 @@ const linkAssets = () => {
       href: "https://fonts.googleapis.com/css?family=Roboto+Condensed",
       type: "text/css",
     },
-    { rel: "stylesheet", href: "/assets/styles/main.css" },
-    // SEO: If your mobile URL is different from the desktop URL,
-    // add a canonical link to the desktop page https://developers.google.com/webmasters/smartphone-sites/feature-phones
-    // { 'rel': 'canonical', 'href': 'http://www.example.com/' }
+    { rel: "stylesheet", href: "/assets/styles/main.css" }
   ];
   return isProduction ? links : links.filter(l => l.rel !== "stylesheet");
 };
 
-export const title = "reactGo - just ship it";
+export const title = "Life manager - control your life";
 export const meta = metaAssets();
 export const link = linkAssets();
